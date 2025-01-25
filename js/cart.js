f5e695e69343f9df0d0d4c24fbc8e6730ab6d1eb
Ceclez <jesus.cecena@uabc.edu.mx>
2025-01-24 22:20:33 -0800

    agregado js, imagenes y css

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let cart_icon = document.querySelector('.header__icon--cart') ,
+        cart_container = document.querySelector('.cart__container') ;
+
+    cart_icon.addEventListener('click', function(event) {
+        event.stopPropagation() ;
+        if (cart_container.style.display === 'none' || cart_container.style.display === '') {
+            cart_container.style.display = 'flex' ;
+        } else {
+            cart_container.style.display = 'none' ;
+        }
+    }) ;
+})
---
+++ styles/styles-II/readme.txt
@@ -0,0 +1,4 @@
+//-----------------//
+
+La hoja de estilos (styles.css) en esta carpeta son los antes necesitados, ahora remplazados 
+por la versión hecha con SASS.
